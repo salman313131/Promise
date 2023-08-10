6322a0282cb305d40736188fba5b4bd2be2b1228
salman <salmankhan.faze@gmail.com>
2023-08-10 14:49:58 +0530

    final async

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 const users = ['user1','user2']
 const last_activity_time={time:new Date()}
-
+console.log(users)
 
 const createUserAndDeleteUser = async()=>{
     console.log(`last activity time : ${last_activity_time.time}`)
-    console.log(users)
     const lastActivity=new Promise((resolve,reject)=>{
         setTimeout(()=>{
             last_activity_time.time = new Date()
             resolve('after user time : '+last_activity_time.time)
-        },1000)
+        },10000)
         })
-    const createUser = new Promise((resolve,reject)=>{
-        users.push('user3')
+    const createUser = (user)=>{
+        return new Promise((resolve,reject)=>{
+        users.push(user)
         resolve('user created')
-    })
+    })}
+    const [afterCreationTime,userCreated] = await Promise.all([lastActivity,createUser('user3')])
+    console.log(users)
+    console.log(afterCreationTime)
     const deleteUser = new Promise((res,rej)=>{
         const user = users.pop()
         res(user)
     })
-    const [afterCreationTime,userCreated] = await Promise.all([lastActivity,createUser])
-    console.log(users)
-    console.log(afterCreationTime)
-    const [afterDeletionTime,userDeleted] = await Promise.all([lastActivity,deleteUser])
+    const userDeleted = await deleteUser
     console.log(`user deleted is ${userDeleted}`)
-    console.log(afterDeletionTime)
-    console.log(users)
 }
 
 
-createUserAndDeleteUser()
+createUserAndDeleteUser().then(()=>{
+    console.log(users)
+})
